95559aa01f2ab294d50a5cd68dd660669b9c734d
Nick van Santen <vansantennick@gmail.com>
2021-04-01 18:15:03 +0200

    Program saves and displays a simple chart

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace MCRT
 {
@@ -6,7 +9,26 @@ namespace MCRT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Chart chart = new Chart();
+            chart.Size = new System.Drawing.Size(640, 320);
+            chart.ChartAreas.Add("ChartArea1");
+            chart.Legends.Add("legend1");
+
+            // Add Series with legend
+            chart.Series.Add("sin");
+            chart.Series["sin"].LegendText = "Sin(x)";
+            chart.Series["sin"].ChartType = SeriesChartType.Spline;
+
+            // Add datapoints
+            for (double x = 0; x < 2 * Math.PI; x += 0.01)
+            {
+                chart.Series["sin"].Points.AddXY(x, Math.Sin(x));
+            }
+
+            // Save and open chart
+            string chartPath = "sin_0_2pi.png";
+            chart.SaveImage(chartPath, ImageFormat.Png);
+            Process.Start(chartPath);
         }
     }
-}
+}   
